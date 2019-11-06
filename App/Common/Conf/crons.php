23db3e536265d38e2b91be49215f13e3ee94dc24
345111458 <345111458@qq.com>
2019-11-06 14:51:59 +0800

    454

--- App/Common/Conf/crons.php
+++ App/Common/Conf/crons.php
@@ -3,6 +3,16 @@
 
  return array(
     'cron_1' => array('recommend', 60), //这里的意思是每隔60秒，执行一次recommend.php文件
+    'cron_1' => array('recommend', 60), //这里的意思是每隔60秒，执行一次recommend.php文件
+    'cron_1' => array('recommend', 60), //这里的意思是每隔60秒，执行一次recommend.php文件
+    'cron_1' => array('recommend', 60), //这里的意思是每隔60秒，执行一次recommend.php文件
+    'cron_1' => array('recommend', 60), //这里的意思是每隔60秒，执行一次recommend.php文件
+    'cron_1' => array('recommend', 60), //这里的意思是每隔60秒，执行一次recommend.php文件
+    'cron_1' => array('recommend', 60), //这里的意思是每隔60秒，执行一次recommend.php文件
+    'cron_1' => array('recommend', 60), //这里的意思是每隔60秒，执行一次recommend.php文件
+    'cron_1' => array('recommend', 60), //这里的意思是每隔60秒，执行一次recommend.php文件
+    'cron_1' => array('recommend', 60), //这里的意思是每隔60秒，执行一次recommend.php文件
+    'cron_1' => array('recommend', 60), //这里的意思是每隔60秒，执行一次recommend.php文件
  );
 // fdsafdsafdsafdsa
 // fdsafdsafdsafdsa
