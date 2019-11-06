49400110c02bd08b0d13e59c56041ac932bce199
345111458 <345111458@qq.com>
2019-11-06 14:50:27 +0800

    1564

--- App/Common/Conf/crons.php
+++ App/Common/Conf/crons.php
@@ -5,5 +5,10 @@
     'cron_1' => array('recommend', 60), //这里的意思是每隔60秒，执行一次recommend.php文件
  );
 // fdsafdsafdsafdsa
+// fdsafdsafdsafdsa
+// fdsafdsafdsafdsa
+// fdsafdsafdsafdsa
+// fdsafdsafdsafdsa
+// fdsafdsafdsafdsa
 
 ?>
